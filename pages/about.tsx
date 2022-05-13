b5ce7da1c78cda266d3dc97fca02157cb97a871a
mi-wada <mtak.wd@gmail.com>
2022-05-13 22:05:59 +0900

    prepare for deploy

---
+++ pages/about.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from 'next'
+import styles from '../styles/Home.module.css'
+
+const About: NextPage = () => {
+  return (
+    <div className={styles.container}>
+      This is about page.
+    </div>
+  )
+}
+
+export default About;
